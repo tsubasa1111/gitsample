c04db985adb1887515ac6a3993321817b67a8afb
tsubasa1111 <namiki.ba13@gmail.com>
2020-02-28 13:46:19 +0900

    firstcommit

---
+++ src/main/java/com/example/GitsampleApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitsampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitsampleApplication.class, args);
+	}
+
+}
